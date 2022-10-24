39e350459b8f8fdc8355572b28e8127125b39127
gtoste <gczyszczon39@gmail.com>
2022-10-24 09:13:02 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+//init
+const canvas : HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("canvas");
+
+const option_download : HTMLButtonElement = <HTMLButtonElement>document.getElementById("option_download");
+const option_clear : HTMLButtonElement = <HTMLButtonElement>document.getElementById("options_clear"); 
+const option_previous : HTMLButtonElement = <HTMLButtonElement>document.getElementById("options_previous");
+
+const option_color_red : HTMLButtonElement = <HTMLButtonElement>document.getElementById("options_color_red");
+const option_color_blue : HTMLButtonElement = <HTMLButtonElement>document.getElementById("options_color_blue");
+const option_color_green : HTMLButtonElement = <HTMLButtonElement>document.getElementById("options_color_green");
+const option_color_yellow : HTMLButtonElement = <HTMLButtonElement>document.getElementById("options_color_yellow");
+const color_picker : HTMLButtonElement = <HTMLButtonElement>document.getElementById("color_picker");
+const option_brush : HTMLButtonElement = <HTMLButtonElement>document.getElementById("options_brush");
+const option_rectangle : HTMLButtonElement = <HTMLButtonElement>document.getElementById("options_rectangle");
+const option_circle : HTMLButtonElement = <HTMLButtonElement>document.getElementById("options_circle");
+const option_line : HTMLButtonElement = <HTMLButtonElement>document.getElementById("options_line");
+const option_proportions : HTMLButtonElement = <HTMLButtonElement>document.getElementById("options_propotions");
+const option_fill : HTMLButtonElement = <HTMLButtonElement>document.getElementById("options_fill");
